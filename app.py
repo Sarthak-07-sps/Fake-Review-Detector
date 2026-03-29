2a0bab067b24bc95a70bf029822a2e655e16427c
Sarthak <sarthak.sk001@gmail.com>
2026-03-29 22:59:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+# app.py
 
+import pickle
+from utils import preprocess_text
+
+model = pickle.load(open("model/model.pkl", "rb"))
+vectorizer = pickle.load(open("model/vectorizer.pkl", "rb"))
+
+def predict_review(text):
+    text = preprocess_text(text)
+    vec = vectorizer.transform([text])
+    return model.predict(vec)[0]
+
+# CLI
+while True:
+    text = input("Enter review: ")
+    result = predict_review(text)
+    print("Prediction:", result)
